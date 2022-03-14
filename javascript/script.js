a932dc9a7a087c5131ea1392bc49549605de8bf9
michele camera <michy.cam95@gmail.com>
2022-03-14 14:54:31 +0100

    creation second exercise

--- javascript/script.js
+++ javascript/script.js
@@ -30,4 +30,75 @@ const value = checkPalindrome(string);
 console.log(value);
 
 
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
 
+
+// l'utente inserisce un numero
+let usernumber = parseInt(prompt("inserici un numero"));
+
+// controllo che il numero immesso dall'utente sia da 1 a 5
+if (usernumber <= 5 && usernumber >= 1) {
+    console.log("Your number (" + usernumber + ")  matches requirements");
+    if (usernumber % 2 === 0) {
+        let numeroDispari
+        console.log("User selected even number!");
+    }
+    else {
+        console.log("User selected odd number!");
+    }
+} else if (isNaN(usernumber)) {
+    console.log("It is not a number. Please enter a number from 1 to 5");
+} else {
+    console.log("Your number (" + usernumber + ") is not between 1 and 5");
+}
+
+// controllo che il numero immesso dall'utente sia pari o dispari
+
+
+//genero il numero random del PC
+
+const pcnumber = generaNumeroRandom(1, 5);
+
+function generaNumeroRandom(min, max) {
+
+    const range = max - min + 1;
+
+    const generatedNumber = Math.floor(Math.random() * range) + min;
+
+    return generatedNumber;
+
+}
+
+//sommo i due numeri
+
+let somma = 0;
+
+somma = pcnumber + usernumber;
+
+//controllo che il risulato sia pari o dispari
+
+function gameResult(number) {
+    if (number % 2 === 0) {
+        return 'Pari';
+    } else {
+        return 'Dispari';
+    }
+}
+
+const evenOdd = gameResult();
+
+console.log("il risultato è:", evenOdd);
+
+//vincitore
+
+if (somma % 2 === 0 && usernumber % 2 === 0) {
+    console.log("vince giocatore!")
+} else if (somma % 2 === 1 && usernumber % 2 === 1) {
+    console.log("vince giocatore!")
+} else {
+    console.log("vince computer!")
+} 
